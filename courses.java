dc58ac3a743b10c81f85573d39c2c15344d0a831
George Pavlidis <georgepaul15@gmail.com>
2020-10-08 20:07:52 +0300

    update

---
+++ courses.java
@@ -0,0 +1,22 @@
+
+public class courses {
+    private int id; 
+    private int semester;
+    private int dm;
+    private String teacher;
+    private String info;
+    
+    public void getId(int x){id=x;}
+    public void getSemester(int x){semester=x;}
+    public void getDm(int x){dm=x;}
+    public void getTeacher(String x){teacher=x;}
+    public void getInfo(String x){info=x;}
+    
+    public int setId(){return id;}
+    public int setSemester(){return semester;}
+    public int setDm(){return dm;}
+    public String setTeacher(){return teacher;}
+    public String setInfo(){return info;}
+    
+    
+}
---
+++ student.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class student {
+    private String am;
+    private int etosEisagogis;
+    private int dm;
+    private String name;
+    private int password;
+    
+    ArrayList mathimata = new ArrayList<courses>();
+    
+    //hsdhs
+    
+}
